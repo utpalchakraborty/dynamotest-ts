06080ca00004835bce7379495cb8d25ea745ce9e
utpalchakraborty@gmail.com <utpalchakraborty@gmail.com>
2022-03-05 14:12:49 -0500

    comments and query example

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import {DynamoDBClient, ListTablesCommand, ScanCommand} from "@aws-sdk/client-dynamodb";
-import { unmarshall } from "@aws-sdk/util-dynamodb"
+import {DynamoDBClient, GetItemCommand, ListTablesCommand, ScanCommand} from "@aws-sdk/client-dynamodb";
+import { unmarshall, marshall } from "@aws-sdk/util-dynamodb"
 import * as dotenv from 'dotenv'
 
 dotenv.config()
@@ -14,15 +14,30 @@ const dynamodb = new DynamoDBClient({
 });
 
 export const handler = async () => {
+
+    // list the tables
     const tablesResponse2 = await dynamodb.send(new ListTablesCommand({
         Limit: 10
     }));
     console.log(tablesResponse2);
 
+    // scan all rows in the table
     const rows = await dynamodb.send(new ScanCommand({TableName: "auth_data"}));
-
     rows.Items?.forEach(
         (item) => console.log(unmarshall(item)))
+
+    // scan for a particular type of row in the table
+    const row = await dynamodb.send(new GetItemCommand({
+        TableName: "auth_data",
+        Key: marshall({
+            "app": "cmp",
+            "user": "utpal"
+        })
+    }))
+    if (row.Item != undefined) {
+        console.log(unmarshall(row.Item))
+    }
+
 };
 
 handler()
